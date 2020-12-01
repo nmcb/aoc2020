a7b1215ec2a29d0f20136de590ae9d8de6d9eeab
nmcb <marco@splatter.nl>
2020-12-01 13:33:55 +0100

    generalised to number of args

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -9,23 +9,30 @@ object Day01 {
       .map(_.toInt)
       .toSet
 
-  val solutionsPart1: Set[Set[Int]] =
-    for {
-      a <- input
-      b <- input
-      if (a + b == 2020)
-    } yield Set(a, b)
+  val tabulated3: Set[Set[Int]] =
+    for { a <- input ; b <- input ; c <- input ; } yield Set(a, b, c)
 
-  val solutionsPart2: Set[Set[Int]] =
-    for {
-      a <- input
-      b <- input
-      c <- input
-      if (a + b + c == 2020)
-    } yield Set(a, b, c)
+  val mul: Set[Int] => Int =
+    _.foldLeft(1)(_*_)
   
+  val sum: Set[Int] => Int =
+    _.foldLeft(0)(_+_)
+
+  def byNumberOfArgs(n: Int): Set[Int] => Boolean =
+    _.size == n
+
+  def bySumEquals(n: Int): Set[Int] => Boolean =
+    sum(_) == n
+
+  def answerByNumberOfArgs(n: Int): Option[Int] =
+    tabulated3
+      .filter(byNumberOfArgs(n))
+      .filter(bySumEquals(2020))
+      .map(mul)
+      .headOption
+    
   def main(args: Array[String]): Unit = {
-    println(s"Answers part 1: ${solutionsPart1.map(_.foldLeft(1)(_*_))}")
-    println(s"Answers part 2: ${solutionsPart2.map(_.foldLeft(1)(_*_))}")
+    println(s"Answer part 1: ${answerByNumberOfArgs(2)}")
+    println(s"Answer part 2: ${answerByNumberOfArgs(3)}")
   }
 }
