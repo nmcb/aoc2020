cf5f2107a9476675e486ff45686cf06afdf8d0bf
nmcb <marco@splatter.nl>
2020-12-02 15:01:14 +0100

    employ library functions

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -2,33 +2,19 @@ import scala.io._
 
 object Day01 {
 
-  val input: Set[Int] =
+  val input: List[Int] =
     Source
       .fromFile("src/resources/input01.txt")
       .getLines
       .map(_.toInt)
-      .toSet
-
-  val distinct3: Set[Set[Int]] =
-    for { a <- input ; b <- input ; c <- input ; } yield Set(a, b, c)
-
-  val mul: Set[Int] => Int =
-    _.foldLeft(1)(_*_)
-  
-  val sum: Set[Int] => Int =
-    _.foldLeft(0)(_+_)
-
-  def byNumberOfArgs(n: Int): Set[Int] => Boolean =
-    _.size == n
-
-  def bySumEquals(n: Int): Set[Int] => Boolean =
-    sum(_) == n
+      .toList
 
   def answer(numberOfArgs: Int)(sumEquals: Int): Option[Int] =
-    distinct3
-      .filter(byNumberOfArgs(numberOfArgs))
-      .filter(bySumEquals(sumEquals))
-      .map(mul)
+    input
+      .combinations(numberOfArgs)
+      .filter(_.sum == sumEquals)
+      .map(_.product)
+      .toList
       .headOption
     
   def main(args: Array[String]): Unit = {
