f88a59354d81fc05486acc07d59d3e6e60a2216b
nmcb <marco@splatter.nl>
2020-12-06 09:00:54 +0100

    grooming chore

--- src/main/scala/Day06.scala
+++ src/main/scala/Day06.scala
@@ -1,6 +1,6 @@
 import scala.io._
 
-object Day06 {
+object Day06 extends App {
 
   val input: List[String] =
     Source
@@ -13,21 +13,23 @@ object Day06 {
       case (acc, line) if line.nonEmpty => (line :: acc.head) :: acc.tail
       case (acc, _) => List.empty[String] :: acc
     }
+
+  def chars(list: List[String]): String =
+    list
+      .fold("")(_+_)
+      .distinct
     
-  val answer1: List[Int] =
+  val answer1: Int =
     answers
-      .map(_.foldLeft("")(_+_).toSet)
+      .map(chars)
       .map(_.size)
+      .sum
 
-  val answer2: List[Int] =
-    answers.map(grp => {
-      val all = grp.foldLeft("")(_+_).toSet
-      val dis = grp.map(_.toSet).foldLeft(all)(_ intersect _)
-      dis.size
-    })
+  val answer2: Int =
+    answers
+      .map(grp => grp.fold(chars(grp))(_ intersect _).size)
+      .sum
 
-  def main(args: Array[String]): Unit = {
-    println(s"Answer part 1: ${answer1.sum}")
-    println(s"Answer part 2: ${answer2.sum}")
-  }
+  println(s"Answer part 1: ${answer1}")
+  println(s"Answer part 2: ${answer2}")
 }
