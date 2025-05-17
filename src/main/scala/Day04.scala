9b2c7f8236aee6e7e9324a9e805a279dcc40ecc4
nmcb <nmcb@xs4all.nl>
2025-05-17 13:52:45 +0200

    cleanup day 04

--- src/main/scala/Day04.scala
+++ src/main/scala/Day04.scala
@@ -5,7 +5,10 @@ object Day04 extends App:
   val day: String = getClass.getSimpleName.filter(_.isDigit).mkString
 
   type Passport = Map[String,String]
-  val  Passport = Map.empty[String,String]
+
+  object Passport:
+    val empty: Passport =
+      Map.empty[String,String]
 
   val input =
       Source
@@ -13,15 +16,14 @@ object Day04 extends App:
         .getLines
         .toList
 
-  val passports: List[Passport] =
-    input.foldLeft(List(Passport)) { (acc,line) =>
-      line.split(' ') match {
+  val passports: Vector[Passport] =
+    input.foldLeft(Vector(Passport.empty)): (acc,line) =>
+      line.split(' ') match
         case Array("") =>
-          Passport :: acc
-        case fs: Array[String] =>
-          fs.map(_.split(':')).map(kv => kv(0) -> kv(1)).toMap ++ acc.head :: acc.tail
-      }
-    }
+          Passport.empty +: acc
+        case line: Array[String] =>
+          val update = line.map(_.split(':')).map(kv => kv(0) -> kv(1)).toMap
+          (update ++ acc.head) +: acc.tail
 
   def valYear(key: String, min: Int, max: Int)(p: Passport): Boolean =
     p.get(key) match {
@@ -72,19 +74,24 @@ object Day04 extends App:
     }
   }
 
-  val answer1 =
-    passports.filter(p => p.size == 8 || (p.size == 7 && !p.keys.toList.contains("cid")))
+  def solve1(passports: Vector[Passport]): Vector[Passport] =
+    passports.filter(p => p.size == 8 || (p.size == 7 && !p.keys.exists(_  == "cid")))
 
-  val answer2 = 
-    answer1
-      .filter(valYear("byr", 1920, 2020))     
+
+  val start1  = System.currentTimeMillis
+  val answer1 = solve1(passports).size
+  println(s"Day $day answer part 1: $answer1 [${System.currentTimeMillis - start1}ms]")
+
+  def solve2(passports: Vector[Passport]): Vector[Passport] =
+    solve1(passports)
+      .filter(valYear("byr", 1920, 2020))
       .filter(valYear("iyr", 2010, 2020))
       .filter(valYear("eyr", 2020, 2030))
       .filter(valHgt)
       .filter(valHcl)
       .filter(valEcl)
       .filter(valPid)
-    
-          
-  println(s"Answer part 1: ${answer1.size}")
-  println(s"Answer part 2: ${answer2.size}")
+
+  val start2  = System.currentTimeMillis
+  val answer2 = solve2(passports).size
+  println(s"Day $day answer part 2: $answer2 [${System.currentTimeMillis - start2}ms]")
