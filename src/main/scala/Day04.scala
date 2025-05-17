395c58b588595e68df7abec692592c78085e0588
nmcb <nmcb@xs4all.nl>
2025-05-17 14:03:36 +0200

    indentation and functional style

--- src/main/scala/Day04.scala
+++ src/main/scala/Day04.scala
@@ -25,58 +25,32 @@ object Day04 extends App:
           val update = line.map(_.split(':')).map(kv => kv(0) -> kv(1)).toMap
           (update ++ acc.head) +: acc.tail
 
-  def valYear(key: String, min: Int, max: Int)(p: Passport): Boolean =
-    p.get(key) match {
-      case Some(str) =>
-        str.toInt >= min && str.toInt <= max
-      case _ =>
-        false
-    }
-    
-  def valHgt(p: Passport): Boolean =
-    p.get("hgt") match {
-      case Some(ht) =>
-        val h = ht.takeWhile(_.isDigit).toInt
-        if      (ht.endsWith("cm"))  h >= 150 && h <= 193
-        else if (ht.endsWith("in"))  h >=  59 && h <=  76
-        else                         false
-      case _ =>
-        false
-    }
+  def valYear(key: String, min: Int, max: Int)(passport: Passport): Boolean =
+    passport.get(key).exists(year => year.toInt >= min && year.toInt <= max)
 
-  def valHcl(p: Passport): Boolean = {
+  def valHgt(p: Passport): Boolean =
+    p.get("hgt").exists: height =>
+      val value = height.takeWhile(_.isDigit).toInt
+      if      height.endsWith("cm") then value >= 150 && value <= 193
+      else if height.endsWith("in") then value >=  59 && value <=  76
+      else false
+
+  def valHcl(p: Passport): Boolean =
     val chars = "0123456789abcdef"
-    p.get("hcl") match {
-      case Some(hc) if hc.startsWith("#") =>
-        hc.drop(1).filter(chars.contains).size == 6
-      case _ =>
-        false
-    }
-  }
-          
-  def valEcl(p: Passport): Boolean = {
+    p.get("hcl").exists: hc =>
+      hc.startsWith("#") && hc.drop(1).count(chars.contains) == 6
+
+  def valEcl(p: Passport): Boolean =
     val colors = List("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
-    p.get("ecl") match {
-      case Some(col) =>
-        colors.contains(col)
-      case None =>
-        false
-    }
-  }
-          
-  def valPid(p: Passport): Boolean = {
+    p.get("ecl").exists(colors.contains)
+
+  def valPid(p: Passport): Boolean =
     val digits = "0123456789"
-    p.get("pid") match {
-      case Some(id) =>
-        id.filter(digits.contains).size == 9
-      case _ =>
-        false
-    }
-  }
+    p.get("pid").exists(_.count(digits.contains) == 9)
+
 
   def solve1(passports: Vector[Passport]): Vector[Passport] =
     passports.filter(p => p.size == 8 || (p.size == 7 && !p.keys.exists(_  == "cid")))
-
 
   val start1  = System.currentTimeMillis
   val answer1 = solve1(passports).size
